1cfa38dfa555fb4941c68d022a8be2f61f9cb171
Matthew Cullen <m23.cullen@gmail.com>
2021-01-26 17:58:00 -0500

    first commit, both solutions passed

---
+++ lesson6Activityp1-2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using MyNamespace.MyObjects;
+
+namespace Transportation.Vehicles
+{
+    public class Auto
+    {
+        public Auto()
+        {
+            this.make = "";
+            this.model = "";
+        }
+
+        public Auto(string make, string model)
+        {
+            this.make = make;
+            this.model = model;
+        }
+
+        public string make { get; set; }
+        public string model { get; set; }
+    }
+}
+
+namespace Exercise
+{
+
+    public class Test
+    {
+        // TODO: 'TestWidget` method
+        public void TestWidget(Widget widget)
+        {
+            widget.viewed++;
+        }
+    }
+}
+
+namespace MyNamespace
+{
+    namespace MyObjects
+    {
+        public class Widget
+        {
+            public int viewed;
+
+            public Widget()
+            {
+                this.viewed = 0;
+            }
+        }
+    }
+}
